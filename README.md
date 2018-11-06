22b20976f213304712b2418c5c6a2139ee665b72
Jacob Boisseau <jacobboisseau@gmail.com>
2018-11-05 20:55:29 -0500

    project finished

---
+++ README.md
@@ -0,0 +1,5 @@
+Installing
+pip install numpy scipy scikit-learn scikit-surprise
+
+Running
+python3 hw4.py
--- hw4_try2.py
+++ hw4_try2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import random
+import heapq
+
 from surprise import AlgoBase
 from surprise import Dataset
 from surprise import Reader
@@ -12,70 +14,64 @@ training_set = pd.read_csv('additional_files/train.dat',delim_whitespace=True)
 test_set = pd.read_csv('1540226926_0229266_test.dat',delim_whitespace=True)
 
 
-class KNNBasic(SymmetricAlgo):
-    def __init__(self, k=40, min_k=1, sim_options={}, verbose=True, **kwargs):
-
-        SymmetricAlgo.__init__(self, sim_options=sim_options, verbose=verbose,
-                               **kwargs)
-        self.k = k
-        self.min_k = min_k
+class customRecommender(SymmetricAlgo):
+    def __init__(self):
+        SymmetricAlgo.__init__(self)
+        self.k = 40
+        self.min_k = 1
 
     def fit(self, trainset):
-
         SymmetricAlgo.fit(self, trainset)
         self.sim = self.compute_similarities()
-
         return self
 
     def estimate(self, u, i):
-
-        import heapq
         # checks if there is data for given user and movie IDs
         if not (self.trainset.knows_user(u) and self.trainset.knows_item(i)):
             raise PredictionImpossible('User and/or item is unkown.')
+
         x, y = self.switch(u, i)
+        # gets the nearest neighbors for each user
         neighbors = [(self.sim[x, x2], r) for (x2, r) in self.yr[y]]
         k_neighbors = heapq.nlargest(self.k, neighbors, key=lambda t: t[0])
 
-        # compute weighted average
-        sum_sim = 0
-        sum_ratings = 0
-        actual_k = 0
+        total_similarity = 0
+        total_ratings = 0
+        current_k = 0
         for (sim, r) in k_neighbors:
             if sim > 0:
-                sum_sim += sim
-                sum_ratings += sim * r
-                actual_k += 1
+                total_similarity += sim
+                total_ratings += sim * r
+                current_k += 1
 
-        est = sum_ratings / sum_sim
-        return est
+        unknown_user_rating = total_ratings / total_similarity
 
-
-
+        return unknown_user_rating
 
 
-algo = KNNBasic()
 
-# A reader is still needed but only the rating_scale param is requiered.
-reader = Reader(rating_scale=(0, 5))
 
-# The columns must correspond to user id, item id and ratings (in that order).
+
+recommender = customRecommender()
+reader = Reader(rating_scale=(0, 5)) # sets min and max of rating system
+
+# reads in training set and configes it for recommending
 data = Dataset.load_from_df(training_set[['userID', 'movieID', 'rating']], reader)
-
 trainset = data.build_full_trainset()
+recommender.fit(trainset)
 
-algo.fit(trainset)
 results = []
+print('🎯 Working... 🎯')
+# rates each test input
 for i, test_row in test_set.iterrows():
     test_userID = test_row['userID']
     test_movieID = test_row['movieID']
-    pred = algo.predict(test_userID, test_movieID,verbose=False).est
+    pred = recommender.predict(test_userID, test_movieID).est
     results.append(pred)
 
 
-print(results)
-# output_file = str('test'+str(random.randint(1,1001))+'.dat')
-# file = open(output_file,'w')
-# for i in range(0,len(results)):
-#     file.write(str(results[i]) + '\n')
-# print('Finished! Results in ➡️',output_file)
+output_file = str('rating_output'+str(random.randint(1,1001))+'.dat')
+file = open(output_file,'w')
+for i in range(0,len(results)):
+    file.write(str(results[i]) + '\n')
+print('Finished! Results in ➡️',output_file)
